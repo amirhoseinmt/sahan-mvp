9dd96590bc4091eb2fcee36fa6e4c7025b9c2516
amirhosein mt <amirhoseinmirtalemi@gmail.com>
2025-05-07 10:11:25 +0330

    edit sign in page

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -10,6 +10,28 @@
 // export default Page
 
 // app/sign-in/page.tsx
+
+// "use client";
+// import { SignIn } from "@clerk/nextjs";
+// import { useUser } from "@clerk/nextjs";
+// import { useRouter } from "next/navigation";
+// import { useEffect } from "react";
+
+// export default function SignInPage() {
+//   const { isLoaded, isSignedIn } = useUser();
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     if (isLoaded && isSignedIn) {
+//       router.push("/dashboard"); // or your initial redirect logic
+//     }
+//   }, [isLoaded, isSignedIn]);
+
+//   if (!isLoaded || isSignedIn) return null;
+
+//   return <SignIn />;
+// }
+
 "use client";
 import { SignIn } from "@clerk/nextjs";
 import { useUser } from "@clerk/nextjs";
@@ -17,14 +39,15 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function SignInPage() {
-  const { isLoaded, isSignedIn } = useUser();
+  const { isLoaded, isSignedIn, user } = useUser();
   const router = useRouter();
 
   useEffect(() => {
-    if (isLoaded && isSignedIn) {
-      router.push("/dashboard"); // or your initial redirect logic
+    if (isLoaded && isSignedIn && user) {
+      const slug = `${user.firstName ?? ""}${user.lastName ?? ""}`;
+      router.push(`/dashboard/${slug}`);
     }
-  }, [isLoaded, isSignedIn]);
+  }, [isLoaded, isSignedIn, user]);
 
   if (!isLoaded || isSignedIn) return null;
 
